eb90b2db67171276731ef1dc5ef75da676260a26
Eusebi Jucglà <31617263+eusebijucgla@users.noreply.github.com>
2026-02-04 12:10:35 +0100

    Created simple filters to use with the Router Interface.

---
+++ Source/OSCToolset/Private/Functions/OSCT_Filters.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Functions/OSCT_Filters.h"
+
+#include "OSCToolsetLog.h"
+
+void UOSCT_Filters::FilterByAddress(const FOSCT_Module& InModule, const FString& MatchAddress, EOSCT_FilterResult& Branches)
+{
+	// const FString& ReceiverFullAddr = InModule.GetFormattedAddress();
+    
+	if (InModule.FormattedAddress.IsEmpty())
+	{
+		Branches = EOSCT_FilterResult::NoMatch;
+		return;
+	}
+	
+	// Comparing references is much faster than copying then comparing
+	if (InModule.FormattedAddress == MatchAddress)
+	{
+		Branches = EOSCT_FilterResult::Matches;
+		return;
+	}
+
+	// Check if the "Clean" version matches (User passed a raw address like "my/path")
+	// We compare the end of the Receiver's full address to the MatchAddress
+	if (InModule.FormattedAddress.EndsWith(InModule.Address))
+	{
+		Branches = EOSCT_FilterResult::Matches;
+		return;
+	}
+
+	Branches = EOSCT_FilterResult::NoMatch;
+}
+
+bool UOSCT_Filters::FilterByModule(const FOSCT_Module& InModule, const FOSCT_Module& MatchModule, EOSCT_FilterResult& Branches)
+{
+	// These calls will now trigger the calculation automatically if needed
+	// const FString& AddrA = InModule.GetFormattedAddress();
+	// const FString& AddrB = MatchModule.GetFormattedAddress();
+
+	const bool bMatches = (!InModule.FormattedAddress.IsEmpty() && InModule.FormattedAddress == MatchModule.FormattedAddress);
+	UE_LOG(OSCToolset, Log, TEXT("In: %s Match: %s"), *InModule.FormattedAddress, *MatchModule.FormattedAddress)
+    
+	Branches = bMatches ? EOSCT_FilterResult::Matches : EOSCT_FilterResult::NoMatch;
+	return bMatches;
+}
---
+++ Source/OSCToolset/Public/Functions/OSCT_Filters.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "OSCT_ETypes.h"
+
+#include "OSCT_Filters.generated.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class EOSCT_FilterResult : uint8
+{
+	Matches,
+	NoMatch
+};
+
+UCLASS()
+class OSCTOOLSET_API UOSCT_Filters : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "OSCToolset|Filters", meta = (ExpandEnumAsExecs = "Branches"))
+	static void FilterByAddress(const FOSCT_Module& InModule, const FString& MatchAddress, EOSCT_FilterResult& Branches);
+
+	UFUNCTION(BlueprintCallable, Category = "OSCToolset|Filters", meta = (ExpandEnumAsExecs = "Branches"))
+	static bool FilterByModule(const FOSCT_Module& InModule, const FOSCT_Module& MatchModule, EOSCT_FilterResult& Branches);
+};
