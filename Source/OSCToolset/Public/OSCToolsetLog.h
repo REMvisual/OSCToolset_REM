b40807231a52acc1dd25593f9b1cfd7970ac6a82
Eusebi Jucglà <31617263+eusebijucgla@users.noreply.github.com>
2026-02-04 12:13:39 +0100

    Added a log module to avoid circular dependencies.

---
+++ Source/OSCToolset/Public/OSCToolsetLog.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(OSCToolset, Log, All);
+
+// #include "UObject/Object.h"
+// #include "OSCToolsetLog.generated.h"
+//
+// /**
+//  * 
+//  */
+// UCLASS()
+// class OSCTOOLSET_API UOSCToolsetLog : public UObject
+// {
+// 	GENERATED_BODY()
+// };
