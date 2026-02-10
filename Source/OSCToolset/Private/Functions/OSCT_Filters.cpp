b97fd23865ad729101a28404fbd8b0d3a449282f
Eusebi Jucglà <31617263+eusebijucgla@users.noreply.github.com>
2026-02-10 09:38:38 +0100

    Worked on simple filters for the Interface events.

--- Source/OSCToolset/Private/Functions/OSCT_Filters.cpp
+++ Source/OSCToolset/Private/Functions/OSCT_Filters.cpp
@@ -1,47 +1,39 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Functions/OSCT_Filters.h"
+#include "OSCT_Modules.h"
 
 #include "OSCToolsetLog.h"
 
-void UOSCT_Filters::FilterByAddress(const FOSCT_Module& InModule, const FString& MatchAddress, EOSCT_FilterResult& Branches)
+void UOSCT_Filters::FilterByAddress(const FOSCT_Receiver& InModule, const FString& MatchAddress, EOSCT_FilterResult& Branches)
 {
-	// const FString& ReceiverFullAddr = InModule.GetFormattedAddress();
-    
+	
 	if (InModule.FormattedAddress.IsEmpty())
 	{
 		Branches = EOSCT_FilterResult::NoMatch;
 		return;
 	}
-	
-	// Comparing references is much faster than copying then comparing
-	if (InModule.FormattedAddress == MatchAddress)
+	//Exact match for either the full formatted address or the address of the module.
+	if (InModule.FormattedAddress == MatchAddress || InModule.Address == MatchAddress)
 	{
 		Branches = EOSCT_FilterResult::Matches;
 		return;
 	}
-
-	// Check if the "Clean" version matches (User passed a raw address like "my/path")
-	// We compare the end of the Receiver's full address to the MatchAddress
-	if (InModule.FormattedAddress.EndsWith(InModule.Address))
-	{
-		Branches = EOSCT_FilterResult::Matches;
-		return;
-	}
-
 	Branches = EOSCT_FilterResult::NoMatch;
 }
 
-bool UOSCT_Filters::FilterByModule(const FOSCT_Module& InModule, const FOSCT_Module& MatchModule, EOSCT_FilterResult& Branches)
+bool UOSCT_Filters::FilterByReceiver(const FOSCT_Receiver& InReceiver, const FOSCT_Receiver& MatchReceiver, EOSCT_FilterResult& Branches)
 {
-	// These calls will now trigger the calculation automatically if needed
-	// const FString& AddrA = InModule.GetFormattedAddress();
-	// const FString& AddrB = MatchModule.GetFormattedAddress();
+	// We check Address, Role, and ModuleType individually, which is inherent to FormattedAddress.
+	const bool bMatches = InReceiver.Address.Equals(MatchReceiver.Address, ESearchCase::IgnoreCase) && 
+		InReceiver.Role == MatchReceiver.Role &&
+		InReceiver.ModuleType == MatchReceiver.ModuleType;
+	if (bMatches)
+	{
+		Branches = EOSCT_FilterResult::Matches;
+		return true;
+	}
 
-	const bool bMatches = (!InModule.FormattedAddress.IsEmpty() && InModule.FormattedAddress == MatchModule.FormattedAddress);
-	UE_LOG(OSCToolset, Log, TEXT("In: %s Match: %s"), *InModule.FormattedAddress, *MatchModule.FormattedAddress)
-    
-	Branches = bMatches ? EOSCT_FilterResult::Matches : EOSCT_FilterResult::NoMatch;
-	return bMatches;
+	Branches = EOSCT_FilterResult::NoMatch;
+	return false;
 }
