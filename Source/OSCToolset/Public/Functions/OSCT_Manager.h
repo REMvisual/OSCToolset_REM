a26f27e5650b8a0c30625d9d8d15bbba2143359b
Eusebi Jucglà <31617263+eusebijucgla@users.noreply.github.com>
2026-02-10 09:38:08 +0100

    Added a Manager that serves as a placeholder for the user to interact with the Master. That way functions are accessible in any BP context. No need to drag and drop the master.

---
+++ Source/OSCToolset/Public/Functions/OSCT_Manager.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "OSCT_ETypes.h"
+#include "OSCT_Modules.h"
+
+#include "OSCT_Manager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OSCTOOLSET_API UOSCT_Manager : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+private:
+	UFUNCTION()
+	static UOSCT_Master* GetMaster(const UObject* WorldContextObject);
+	
+	UFUNCTION(BlueprintCallable, Category = "OSCToolset", meta = (WorldContext = "WorldContextObject", DefaultToSelf = "Owner", HidePin = "Owner"))
+	static void AddReceiver(UObject* WorldContextObject, FOSCT_Receiver Receiver, UObject* Owner);
+	
+	UFUNCTION(BlueprintCallable, Category = "OSCToolset", meta = (WorldContext = "WorldContextObject", DefaultToSelf = "Owner", HidePin = "Owner"))
+	static void AddManyReceivers(UObject* WorldContextObject, TArray<FOSCT_Receiver> Receivers, UObject* Owner);
+
+	UFUNCTION(BlueprintCallable, Category = "OSCToolset", meta = (WorldContext = "WorldContextObject", DefaultToSelf = "Owner", HidePin = "Owner"))
+	static void RemoveReceiver(UObject* WorldContextObject, FOSCT_Receiver Receiver, UObject* Owner);
+	
+	UFUNCTION(BlueprintCallable, Category = "OSCToolset|Senders", meta = (WorldContext = "WorldContextObject", DefaultToSelf = "Owner", HidePin = "Owner"))
+	static void SendEvent(UObject * WorldContextObject, UPARAM(ref) FOSCT_Sender& Sender, UObject* Owner);
+	
+	UFUNCTION(BlueprintCallable, Category = "OSCToolset|Senders", meta = (WorldContext = "WorldContextObject", DefaultToSelf = "Owner", HidePin = "Owner"))
+	static void SendFloat(UObject * WorldContextObject, UPARAM(ref) FOSCT_Sender& Sender, const float Value, UObject* Owner);
+
+	UFUNCTION(BlueprintCallable, Category = "OSCToolset|Senders", meta = (WorldContext = "WorldContextObject", DefaultToSelf = "Owner", HidePin = "Owner"))
+	static void SendString(UObject * WorldContextObject, UPARAM(ref) FOSCT_Sender& Sender, const FString& Value, UObject* Owner);
+
+};
